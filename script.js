c48a10f6c4631fc522160f9a98fc84036cf1b28c
Arnold Diep <tungd@uw.edu>
2019-10-10 17:25:02 -0700

    questions changed

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
+// Header with 'View Highscores' and timer'
+// Intro with a 'Start Quiz' button to intiate the quiz.
+// Timer starts. Each question has 15 seconds to answer the question. If the timer runs out of time, it will considered the question wrong and skip to the next question.
+// If the question is answered correctly, the correct count increases and stores until the end of the quiz. Alert the user right/wrong.
+// After the quiz if over. Show the final score and prompt the user for their initial and submit the infomation and store it to 'Highscores'. 
+// User has the option to 'Go back' to previous page or 'Clear scoreboard'.
 var score = 0;
+var currentQuestion = 0;
 var startEl = document.querySelector("#start");
+var submitEl = document.querySelector("#submit");
+var questions = [
+    {
+        title: "What's the most effective Poke Ball in the game?",
+        choices: ["Great Ball", "Ultra Ball", "Master Ball", "Timer Ball"],
+        answer: "Master Ball"
+    },
+    {
+        title: "Which of these Pokemon is NOT an Eeveelution?",
+        choices: ["Vapereon", "Leafeon", "Flygon", "Sylveon"],
+        answer: "Flygon"
+    },
+    {
+        title: "What object is Hypno always holding?",
+        choices: ["Twisted Spoon", "Pendulum", "Pocket Watch", "Moon Stone"],
+        answer: "Pendulum"
+    },
+    {
+        title: "What is not a category in the Pokemon contests?",
+        choices: ["Beauty", "Cuteness", "Talent", "Coolness"],
+        answer: "Talent"
+    },
+    {
+        title: "The chance of finding a shiny wild Pokémon is a very low, it being 1 in ______.",
+        choices: ["150", "1234", "8192", "9000"],
+        answer: "8192"
+    }];
 
 startEl.addEventListener("click", function () {
     startQuiz();
 
     function startQuiz() {
-        var questions = [
-            {
-                title: "Which color is not considered to be one of the primary colors of light?",
-                choices: ["Red", "Yellow", "Green", "Blue"],
-                answer: "Yellow"
-            },
-            {
-                title: "Many scientists think that some of the dinosaurs did not go extinct, but rather evolved into what kind of creature?",
-                choices: ["Amphibians", "Reptiles", "Birds", "Mammals"],
-                answer: "Birds"
-            },
-            {
-                title: "Which of the following is not a favorable adjective when discussing wine?",
-                choices: ["Fat", "Flinty", "Leggy", "Vigorous"],
-                answer: "Fat"
-            },
-            {
-                title: "What is the traditional gift for a 40th wedding anniversary?",
-                choices: ["Emerald", "Diamond", "Ruby", "Sapphire"],
-                answer: "Ruby"
-            },
-            {
-                title: "The Declaration of Independence was written by whom?",
-                choices: ["Benjamin Franklin", "Thomas Jefferson", "James Madison", "George Washington"],
-                answer: "Thomas Jefferson"
-            }]
         for (var i = 0; i < questions, length; i++) {
             var response === choices.indexOf[i]; //The answer the user chosen
             if response == questions[i].answer {
@@ -42,13 +50,6 @@ startEl.addEventListener("click", function () {
         }
         alert("Your final score is " + score + ".")
     };
-
-    // Header with 'View Highscores' and timer'
-    // Intro with a 'Start Quiz' button to intiate the quiz.
-    // Timer starts. Each question has 15 seconds to answer the question. If the timer runs out of time, it will considered the question wrong and skip to the next question.
-    // If the question is answered correctly, the correct count increases and stores until the end of the quiz. Alert the user right/wrong.
-    // After the quiz if over. Show the final score and prompt the user for their initial and submit the infomation and store it to 'Highscores'. 
-    // User has the option to 'Go back' to previous page or 'Clear scoreboard'.
 
     var timeEL = document.querySelector(".time");
     var secondsLeft = 15;
@@ -66,4 +67,9 @@ startEl.addEventListener("click", function () {
     }
 
     setTime();
+});
+
+submitEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    //Submit initial and score to scoreboard.
 });
