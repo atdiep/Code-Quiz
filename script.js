078912f371fd624113eaafd83d7283a1c34153e4
Arnold Diep <tungd@uw.edu>
2019-10-11 10:54:40 -0700

    add functionality to the quiz

--- script.js
+++ script.js
@@ -1,75 +1,119 @@
-// Header with 'View Highscores' and timer'
-// Intro with a 'Start Quiz' button to intiate the quiz.
-// Timer starts. Each question has 15 seconds to answer the question. If the timer runs out of time, it will considered the question wrong and skip to the next question.
-// If the question is answered correctly, the correct count increases and stores until the end of the quiz. Alert the user right/wrong.
-// After the quiz if over. Show the final score and prompt the user for their initial and submit the infomation and store it to 'Highscores'. 
-// User has the option to 'Go back' to previous page or 'Clear scoreboard'.
 var score = 0;
+// Track the index where we are in the questions.
 var currentQuestion = 0;
-var startEl = document.querySelector("#start");
-var submitEl = document.querySelector("#submit");
+
 var questions = [
     {
         title: "What's the most effective Poke Ball in the game?",
         choices: ["Great Ball", "Ultra Ball", "Master Ball", "Timer Ball"],
-        answer: "Master Ball"
+        answer: 2
     },
     {
         title: "Which of these Pokemon is NOT an Eeveelution?",
         choices: ["Vapereon", "Leafeon", "Flygon", "Sylveon"],
-        answer: "Flygon"
+        answer: 2
     },
     {
         title: "What object is Hypno always holding?",
         choices: ["Twisted Spoon", "Pendulum", "Pocket Watch", "Moon Stone"],
-        answer: "Pendulum"
+        answer: 1
     },
     {
         title: "What is not a category in the Pokemon contests?",
         choices: ["Beauty", "Cuteness", "Talent", "Coolness"],
-        answer: "Talent"
+        answer: 2
     },
     {
         title: "The chance of finding a shiny wild Pokémon is a very low, it being 1 in ______.",
         choices: ["150", "1234", "8192", "9000"],
-        answer: "8192"
+        answer: 2
     }];
 
-startEl.addEventListener("click", function () {
-    startQuiz();
 
-    function startQuiz() {
-        for (var i = 0; i < questions, length; i++) {
-            var response === choices.indexOf[i]; //The answer the user chosen
-            if response == questions[i].answer {
-                score++;
-                alert("Correct!");
-            } else {
-                alert("Wrong!");
-            }
+$(document).ready(function () {
+
+    $(".start a").on("click", function (event) {
+        event.preventDefault();
+        $(".start").hide();
+        $(".quiz").show();
+        showQuestion();
+    });
+
+    $(".quiz ul").on("click", "li", function () {
+        $(".selected").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $(".quiz a").click(function (event) {
+        event.preventDefault();
+        if ($("li.selected").length) {
+            var guess = parseInt($("li.selected").attr("id"));
+            checkAnswer(guess);
+        } else {
+            alert("Please select an answer.");
         }
-        alert("Your final score is " + score + ".")
+    });
+
+    $(".final a").click(function (event) {
+        event.preventDefault();
+        restartQuiz();
+    });
+})
+
+function showQuestion() {
+    var question = questions[currentQuestion];
+    $(".quiz h2").text(question.title);
+    $(".quiz ul").html("");
+    for (var i = 0; i < question.choices.length; i++) {
+        $(".quiz ul").append("<li id = '" + i + "'> " + question.choices[i])
     };
+}
 
-    var timeEL = document.querySelector(".time");
-    var secondsLeft = 15;
-
-    function setTime() {
-        var timerInterval = setInterval(function () {
-            secondsLeft--;
-            timeEL.textContent = "Time: " + secondsLeft;
-
-            if (secondsLeft === 0) {
-                clearInterval(timerInterval);
-                myQuestions(); // Function that skip to the next question and count the last question inccorect.
-            }
-        }, 1000);
+function checkAnswer() {
+    var question = questions[currentQuestion];
+    if (question.answer === guess) {
+        score++;
     }
+    currentQuestion++;
+    if (currentQuestion >= questions.length) {
+        showSummary();
+    } else {
+        showQuestion();
+    };
+}
 
-    setTime();
-});
+function showSummary() {
+    $(".quiz").hide();
+    $(".summary").show();
+    $(".summary p").text("Congrats you scored " + score + " out of " + questions.length + " correct!")
+}
 
-submitEl.addEventListener("click", function (event) {
-    event.preventDefault();
-    //Submit initial and score to scoreboard.
-});
+function scoreBoard() {
+
+}
+
+function restartQuiz() {
+    $(".summary").hide();
+    $(".quiz").show();
+    score = 0;
+    currentQuestion = 0;
+    showQuestion();
+}
+
+
+var timeEL = document.querySelector(".time");
+var secondsLeft = 15;
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEL.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            myQuestions(); // Function that skip to the next question and count the last question inccorect.
+        }
+    }, 1000);
+}
+
+setTime();
