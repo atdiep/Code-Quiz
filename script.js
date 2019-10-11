213bbbb2bc5c700430a6ad44ec8e67ae62def57b
Arnold Diep <tungd@uw.edu>
2019-10-11 15:22:31 -0700

    fixed functionality and codes able to run. Need to do CSS portion.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+console.log("scriptLoading")
+// Track the number of correct answers
 var score = 0;
 // Track the index where we are in the questions.
 var currentQuestion = 0;
-
+// List of quesions in arrays
 var questions = [
     {
         title: "What's the most effective Poke Ball in the game?",
@@ -29,47 +31,75 @@ var questions = [
         answer: 2
     }];
 
-
+// This code makes sure that the JavaScript doesn't get run until the HTML is finished loading
 $(document).ready(function () {
-
-    $(".start a").on("click", function (event) {
+    
+    $(".quiz").hide();
+    $(".summary").hide();
+    $(".final").hide();
+    // Create an event listener on "Start Quiz". Hide the starting screen to show the quiz.
+    $(".start button").on("click", function (event) {
         event.preventDefault();
         $(".start").hide();
         $(".quiz").show();
         showQuestion();
     });
-
-    $(".quiz ul").on("click", "li", function () {
+    // Create an event listener on li when an option is being selected by user.
+    // $(".quiz ul").on("click", "li", function () {
+    //     $(".selected").removeClass("selected");
+    //     $(this).addClass("selected");
+    // });
+    // Create an event listener on "Submit Answer" and assign a var for user answers.
+    $(".quiz ul").on("click", "li", function (event) {
+        event.preventDefault();
         $(".selected").removeClass("selected");
         $(this).addClass("selected");
-    });
-
-    $(".quiz a").click(function (event) {
-        event.preventDefault();
         if ($("li.selected").length) {
+            console.log("Guess")
             var guess = parseInt($("li.selected").attr("id"));
             checkAnswer(guess);
         } else {
             alert("Please select an answer.");
         }
     });
+    // Create an event listener on "Submit" to store the user initial and score and submit to scoreboard.
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
 
-    $(".final a").click(function (event) {
+        var initial = document.querySelector("#initial").value;
+
+        if (initial === "") {
+            displayMessage("error", "Initial cannot be blank");
+        } else {
+            localStorage.setItem("initial", initial);
+        }
+        $(".highScore").append("<li>" + initial + score + "</li>");
+        showScore();
+    })
+    // Create an event listener on "Restart Quiz" to restart the quiz.
+    $("#restart").on("click", function (event) {
         event.preventDefault();
         restartQuiz();
     });
-})
+    // Create an event listener on "Clear Scoreboard" to clear all scores.
+    $("#clear").on("click", function(event) {
+        $("ul").html("");
+    });
 
+});
+
+// Create a function to show up one at a time.
 function showQuestion() {
     var question = questions[currentQuestion];
-    $(".quiz h2").text(question.title);
+    $(".quiz h3").text(question.title);
     $(".quiz ul").html("");
     for (var i = 0; i < question.choices.length; i++) {
-        $(".quiz ul").append("<li id = '" + i + "'> " + question.choices[i])
+        $(".quiz ul").append("<li id = '" + i + "'> " + question.choices[i] + "</li>");
     };
 }
 
-function checkAnswer() {
+// Create a function that check the user answers. If correct, increases score. If there is no more quesion, immediately go to summary.
+function checkAnswer(guess) {
     var question = questions[currentQuestion];
     if (question.answer === guess) {
         score++;
@@ -82,16 +112,18 @@ function checkAnswer() {
     };
 }
 
+// Create a function that show the overall summary.
 function showSummary() {
     $(".quiz").hide();
     $(".summary").show();
     $(".summary p").text("Congrats you scored " + score + " out of " + questions.length + " correct!")
 }
 
-function scoreBoard() {
-
+function showScore() {
+    $(".summary").hide();
+    $(".final").show();
 }
-
+// Create a function that restart the quiz when requested by user.
 function restartQuiz() {
     $(".summary").hide();
     $(".quiz").show();
@@ -100,20 +132,21 @@ function restartQuiz() {
     showQuestion();
 }
 
+// Create a function that allows 15 seconds per question. If timer runs out, question counts as incorrect and skip to the next question.
 
-var timeEL = document.querySelector(".time");
-var secondsLeft = 15;
+// var timeEL = document.querySelector(".time");
+// var secondsLeft = 15;
 
-function setTime() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeEL.textContent = "Time: " + secondsLeft;
+// function setTime() {
+//     var timerInterval = setInterval(function () {
+//         secondsLeft--;
+//         timeEL.textContent = "Time: " + secondsLeft;
 
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            myQuestions(); // Function that skip to the next question and count the last question inccorect.
-        }
-    }, 1000);
-}
+//         if (secondsLeft === 0) {
+//             clearInterval(timerInterval);
+//             myQuestions(); // Function that skip to the next question and count the last question inccorect.
+//         }
+//     }, 1000);
+// }
 
-setTime();
+// setTime();
